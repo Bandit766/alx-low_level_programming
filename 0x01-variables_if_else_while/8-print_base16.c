23883072ea736b5a719edd1f7f40265f9e7401f1
Bandit766 <cameron766@gmail.com>
2022-07-05 10:14:51 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int ch;
+	char albt[] = "123456789abcdef";
+
+	for (ch = 0 ; ch < 16 ; ch++)
+	{
+	putchar(albt[ch]);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
