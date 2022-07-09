bf58ea4605de64b667d9bd5bf72f223fc43ed9b9
Bandit766 <cameron766@gmail.com>
2022-07-09 02:50:49 -0700

    print alphabet task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
