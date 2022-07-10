efcadbb728b14bff79f96d800ef21fb713034e0e
Bandit766 <cameron766@gmail.com>
2022-07-09 17:49:36 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - first 50 fibonacci
+*
+* Return: 0 on success
+*/
+
+int main(void)
+{
+int temp;
+int fib1, fib2, check = 1;
+int sum = 0;
+
+while (check < 4000000 && check > 0)
+{
+	if (fib2 % 2 == 0)
+	{
+		sum += fib2;
+	}
+	temp = fib1 + fib2;
+	fib1 = fib2;
+	fib2 = temp;
+	check = fib1 + fib2;
+}
+printf("%d\n", sum);
+return (0);
+}
