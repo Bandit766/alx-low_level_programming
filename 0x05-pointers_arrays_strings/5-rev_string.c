c3bbf50298dffa0a97f00588d10eea8ef4ef173b
Bandit766 <cameron766@gmail.com>
2022-07-11 13:38:27 -0700

    reverse string rewrite

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* rev_string - reverses the input string
+*
+* @s: input string
+*
+*/
+
+void rev_string(char *s)
+{
+	int l = 0, i = 0, j = 0;
+	char str[20];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		str[i] = s[i];
+		l++;
+	}
+
+
+	for (i = l - 1; i >= 0; i--)
+	{
+		s[j] = str[i];
+		j++;
+	}
+}
