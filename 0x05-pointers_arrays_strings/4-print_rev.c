633b826935c45c9f8e7fd19b9a6a93ca2db368ba
Bandit766 <cameron766@gmail.com>
2022-07-11 07:13:35 -0700

    reverse string task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_rev - prints the input string in reverse
+*
+* @s: input string
+*
+*/
+
+void print_rev(char *s)
+{
+	int l = 0, i = 0;
+
+	while (s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	for (i = l; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
