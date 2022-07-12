51cea62357990f43cd214655206e7e4f89e0ed5e
Bandit766 <cameron766@gmail.com>
2022-07-12 12:46:37 -0700

    fixed issue adding values to char

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * more_numbers - prints numbers 0 to 14 - 10 times
 *
@@ -17,15 +17,16 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
 				_putchar('1');
-			}
+
 			if (j != 0)
-			{
-				c += j;
-			}
+				c = '0' + (j % 10);
+
+			if (j == 0)
+				c = '0';
+
 			_putchar(c);
-	
+				
 		}
 		_putchar('\n');
 	}
