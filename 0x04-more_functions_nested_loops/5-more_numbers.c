cc2de971332a75a6dcc8d6084bcd153caa9a9c91
Bandit766 <cameron766@gmail.com>
2022-07-12 12:48:54 -0700

    betty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 {
 	int i, j;
 	char c = '0';
-		
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
@@ -26,7 +26,6 @@ void more_numbers(void)
 				c = '0';
 
 			_putchar(c);
-				
 		}
 		_putchar('\n');
 	}
