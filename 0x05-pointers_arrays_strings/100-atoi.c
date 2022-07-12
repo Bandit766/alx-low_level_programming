c61dc3d86f738ae68421c4167e414db705a435d9
Bandit766 <cameron766@gmail.com>
2022-07-12 08:46:13 -0700

    sum of chars task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - returns the sum of the numbers in the input string
+ *
+ * @s: input string
+ *
+ * Return: the sum of the numbers in the string, 0 if no numbers
+ */
+
+int _atoi(char *s)
+{
+	int pos = 0, neg = 0, i = 0, digits = 0, sum = 0;
+
+	while (s[i] != '\0')
+	{
+
+
+		/**
+		 * printf("Currently working on the char: %c at pos %d.
+		 * There are %d digits and the sum is %d\n", s[i], i, digits, sum);
+		 */
+		if (s[i] == '+')
+			pos++;
+		if (s[i] == '-')
+			neg++;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (sum == 0)
+			{
+				sum += (int)(s[i]);
+			}
+			else
+			{
+				sum = sum * 10;
+				sum += (int)(s[i]);
+			}
+		}
+
+		i++;
+	}
+
+	if (neg - pos > 0)
+		sum = sum * -1;
+
+	return (sum);
+}
