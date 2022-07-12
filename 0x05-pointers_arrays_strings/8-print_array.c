3fe9c8887c3f6f87998acccfeff5f3fdc657759a
Bandit766 <cameron766@gmail.com>
2022-07-12 07:41:01 -0700

    handles incorrect array length

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,19 +14,20 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n < 1)
+	{	
+		fprintf(stderr, "Invalid array or array length\n");
+	}
+	else
 	{
-		if (a[i])
+
+		for (i = 0; i < n; i++)
 		{
+
 			if (i < n - 1)
 			printf("%d, ", a[i]);
 			else
 			printf("%d\n", a[i]);
-		}
-		else
-		{
-			fprintf(stderr, "Invalid array or array length\n");
-			break;
 		}
 	}
 }
