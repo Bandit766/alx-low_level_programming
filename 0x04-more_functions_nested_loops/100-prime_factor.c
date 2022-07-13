5ef3c7f5a11ec097fedd23acca9cd9d4adb09b31
Bandit766 <cameron766@gmail.com>
2022-07-13 02:08:38 -0700

    test cast to int

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	long n = 612852475143, n1 = n / 3, i, ret = 3;
 	long root = (long)sqrtl((long double)n);
+	int test;
 
 	for (i = 5; i < root; i += 2)
 	{
@@ -17,6 +18,7 @@ int main(void)
 			ret = i;
 		}
 	}
-	printf("%ld\n", ret);
+	test = (int)ret;
+	printf("%d\n", test);
 	return (0);
 }
