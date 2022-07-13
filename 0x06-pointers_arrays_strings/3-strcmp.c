d728d8d5600a81d01bbd23e1af2b3a24a33fd7f7
Bandit766 <cameron766@gmail.com>
2022-07-13 07:43:52 -0700

    compare string task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strcmp - compares 2 strings
+* @s1: first string to be input
+* @s2: second string to be input
+* Return: returns the difference between s1 and s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (s1[i] != '\0' && s2[i] != '\0')
+			break;
+		if ((unsigned char)(s1[i]) != (unsigned char)(s2[i]))
+			break;
+	}
+
+	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
+}
