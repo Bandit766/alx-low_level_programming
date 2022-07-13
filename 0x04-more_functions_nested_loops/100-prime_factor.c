59d1b11ed94b0690f4e9fead5da33a107d37aa61
Bandit766 <cameron766@gmail.com>
2022-07-13 01:55:20 -0700

    prime factor task test

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+* main - finds the largest prime number
+* Return: 0 on success
+*/
+int main(void)
+{
+	long n = 612852475143, n1 = n / 3, i, ret = 3;
+	long root = (long)sqrtl((long double)n);
+
+	for (i = 5; i < root; i += 2)
+	{
+		if (n1 % i == 0)
+		{
+			n1 = n1 / i;
+			ret = i;
+		}
+	}
+	printf("%ld\n", ret);
+	return (0);
+}
