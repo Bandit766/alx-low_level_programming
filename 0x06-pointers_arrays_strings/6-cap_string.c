1369293cf01fab1992df2224048775fe4739da8a
Bandit766 <cameron766@gmail.com>
2022-07-13 14:45:52 -0700

    capitalise words task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *cap_string - first letter of words to upper case
+ *@str: input string
+ *Return: the modified string
+ */
+char *cap_string(char *str)
+	{
+		char vals[] = { ' ', '\t', '\n', ',', ';', '.'
+			, '!', '\?', '\"', '(', ')', '{', '}', '\0' };
+		int i, j, increment;
+		char k;
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			for (j = 0; vals[j] != '\0'; j++)
+			{
+				if (str[i] == vals[j])
+				{
+				for (k = 'a'; k <= 'z'; k++)
+				{
+					if (str[i + 1] == k)
+					{
+						increment = 1;
+						str[i + 1] -= 32;
+						break;
+					}
+				}
+				}
+
+				if (increment)
+				{
+					increment = 0;
+					i++;
+					break;
+				}
+			}
+		}
+
+		return (str);
+	}
