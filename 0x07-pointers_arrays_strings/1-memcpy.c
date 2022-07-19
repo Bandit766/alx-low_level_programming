cf6782494811aa646ab248b7ff91463f4cea3234
Bandit766 <cameron766@gmail.com>
2022-07-19 13:26:13 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memcpy - copies n bytes from src to dest
+* @dest: destination string
+* @src: source string
+* @n: number of chars to copy
+* Return: destination string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
